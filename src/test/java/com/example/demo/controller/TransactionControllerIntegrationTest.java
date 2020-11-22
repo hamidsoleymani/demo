0cca7f9d30c6b828abde7cbf786bdcaa985e5154
hamid soleymani <hamid.soleymani@gmx.de>
2020-11-22 22:16:31 +0100

    Added junit end to end tests

--- src/test/java/com/example/demo/controller/TransactionControllerIntegrationTest.java
+++ src/test/java/com/example/demo/controller/TransactionControllerIntegrationTest.java
@@ -1,27 +1,48 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Transaction;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.Matchers.is;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.math.BigDecimal;
 import java.util.List;
 
-//@SpringBootTest
-@RunWith(SpringRunner.class)
-@WebMvcTest(TransactionController.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+//@WebMvcTest(TransactionController.class)
+@AutoConfigureMockMvc
 public class TransactionControllerIntegrationTest {
     @Autowired
     private TransactionController transactionController;
-   // @Autowired
-    //private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    public void TransactionComponentTest() {
-        List<Transaction> allByAccountNumber = transactionController.findAllByAccountNumber("123");
+    public void TransactionComponentTest() throws Exception {
+        mockMvc.perform(get("/transactions/123"))
+                .andExpect(status().isOk())
+                .andExpect( jsonPath("$[0].type", is("type1")))
+                .andExpect( jsonPath("$[0].accountNumber", is("123")))
+                .andExpect( jsonPath("$[0].currency", is("EURO")))
+                .andExpect( jsonPath("$[0].amount", is(100)))
+                .andExpect( jsonPath("$[0].merchantName", is("Apple")))
+                .andExpect( jsonPath("$[0].merchantLogo", is("Apple_Logo")))
+
+                .andExpect( jsonPath("$[1].type", is("type2")))
+                .andExpect( jsonPath("$[1].accountNumber", is("123")))
+                .andExpect( jsonPath("$[1].currency", is("EURO")))
+                .andExpect( jsonPath("$[1].amount", is(200)))
+                .andExpect( jsonPath("$[1].merchantName", is("SAP")))
+                .andExpect( jsonPath("$[1].merchantLogo", is("SAP_Logo")));
     }
 }
--- src/test/java/com/example/demo/service/TransactionServiceTest.java
+++ src/test/java/com/example/demo/service/TransactionServiceTest.java
@@ -1,6 +1,5 @@
 package com.example.demo.service;
 
-import com.example.demo.service.TransactionService;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
